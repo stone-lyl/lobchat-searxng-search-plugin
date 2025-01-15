ecfe916106507a84d392b7ee4ccfdd9512d86e97
rajput-hemant <rajput.hemant2001@gmail.com>
2025-01-15 22:10:33 +0530

    fix: serve static assets when pluging isnt deployed on vercel
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/middleware/serve-static-local.ts
+++ src/middleware/serve-static-local.ts
@@ -2,7 +2,7 @@ import { MiddlewareHandler } from "hono";
 
 export function serveStaticLocal(): MiddlewareHandler {
   return async (c, next) => {
-    if (process.env.NODE_ENV === "development") {
+    if (!process.env.VERCEL) {
       const { serveStatic } = await import("hono/bun");
       return serveStatic({ root: "./public" })(c, next);
     }
